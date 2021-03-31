c512e6fef5c2dd3e7015325067347b680a1070cf
Maëva Layus <layusmaeva@eisti.eu>
2021-03-31 11:00:19 +0200

    séparation card du dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -67,44 +67,7 @@
         <!--CARTES RESTAURANTS-->
         <div id="contenantListeCards">
           <v-list-item v-for="restaurant in allRestaurants" :key="restaurant[0].Name">
-            <v-card class="listeCards">
-              <v-list-item-content class="contenuCards" >
-              <v-row  class="premiereLigne">
-                <v-col cols="5">
-                <v-list-item class="nomRestaurant"><h1>{{restaurant[0].Name}}</h1></v-list-item> 
-                </v-col>
-                <v-col cols="3" class="colCards">
-                  <v-list-item class="notes"><v-rating background-color="orange lighten-3" color="orange" :value="starAverage(restaurant)" half-icon="mdi-star-half" half-increments readonly ></v-rating></v-list-item>
-                </v-col>
-                <v-col cols="4" class="photo">
-                  <v-list-item ><v-img max-height="300" max-width="500" :src="restaurant[0].LogoUrl"></v-img></v-list-item> 
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col cols="3" class="colCards">
-                  <v-list-item class="nomTypes"><font v-for="type in calculTypes(restaurant,[])" :key="type" class="nomType" >{{type}}</font></v-list-item>
-                </v-col>
-                <v-col cols="5" class="colDifLivreurs">
-                  <v-row>
-                    <v-col>
-                      <v-card-title>Délais : </v-card-title>
-                      <v-card-text>
-                          <v-chip class="tempsLivraison" v-for="resto in restaurant" :key="resto.id">{{resto.Api}} : {{resto.DeliveryEtaMinutes.RangeLower}} - {{resto.DeliveryEtaMinutes.RangeUpper}} min</v-chip>
-                      </v-card-text>
-                    </v-col>
-                    <v-col>
-                      <v-card-title>Frais de livraison : </v-card-title>
-                      <v-card-text>
-                          <v-chip class="tempsLivraison" v-for="resto in restaurant" :key="resto.id">{{resto.Api}} : {{resto.DeliveryCost}}{{devise}} </v-chip>
-                      </v-card-text>
-                    </v-col>
-                  </v-row>
-                </v-col>
-                <v-col  cols="3">
-                </v-col>
-              </v-row>
-              </v-list-item-content>
-            </v-card>      
+            <DashboardCard v-bind:restaurant="restaurant" v-bind:devise="devise"></DashboardCard>
           </v-list-item>
         </div>
       </div>
@@ -160,67 +123,23 @@
   width: 75%;
 }
 
-.listeCards{
-  text-align: left;
-  margin:10px;
-  width: 100%;
-}
-
-.nomRestaurant{
-  margin:20px;
-  margin-left: 40px;
-}
-
-.nomTypes{
-  margin-top:20%;
-  margin-left: 60px;
-  color:gray;
-}
-
-.nomType{
-  margin-right: 5px;
-}
-
-.colCards{
-  width:20%;
-}
-
-.notes{
-  margin-top: 20px;
-}
-
-.premiereLigne{
-  height: 30px;
-  width:100%;
-}
-
-.photo{
-  text-align: right;
-  padding:0px;
-  margin:0px;
-}
-
-.colDifLivreurs{
-  margin-top:40px;
-}
-
-.tempsLivraison{
-  margin:5px;
-}
-
 </style>
 
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
 
 <script>
 import axios from 'axios';
+import DashboardCard from './DashboardCard'
 
 export default {
   name: "Dashboard",
+  components: {
+    DashboardCard
+  },
   data: () => ({
       inputCity: "", //adresse
       numAdresses: "",
-      pays: ["France", "Royaume-Unis"],
+      pays: ["France", "Royaume-Uni"],
       PaysChoisit: "",
       suggestionsHere: [], // Tableau qui contiendra les suggestions Here
       suggestionSelected: "", // Adresse selectionnées
@@ -362,28 +281,6 @@ export default {
     filtrerRecherche(){
 
     },
-    calculTypes(sameRestaurants,res){
-      var typesSameRestaurant=[];
-      for(var restaurantNum in sameRestaurants){
-        for(var typeNum in sameRestaurants[restaurantNum].CuisineTypes){
-          typesSameRestaurant.push(sameRestaurants[restaurantNum].CuisineTypes[typeNum].Name)
-        }
-      }
-      var res = typesSameRestaurant.filter(this.listeUnique)
-      return res
-    },
-    listeUnique(value, index, self) {
-      return self.indexOf(value) === index;
-    },
-    starAverage(sameRestaurants){
-      var average=0;
-      for(var restaurantNum in sameRestaurants){
-        average=average+sameRestaurants[restaurantNum].Rating.StarRating
-      }
-      average=average/sameRestaurants.length
-      console.log(average)
-      return average
-    }
   },
 };
 </script>
---
+++ src/components/DashboardCard.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+        <v-card class="listeCards">
+            <v-list-item-content class="contenuCards" >
+                <v-row  class="premiereLigne">
+                    <v-col cols="5">
+                        <v-list-item class="nomRestaurant"><h1>{{restaurant[0].Name}}</h1></v-list-item> 
+                    </v-col>
+                    <v-col cols="3" class="colCards">
+                        <v-list-item class="notes"><v-rating background-color="orange lighten-3" color="orange" :value="starAverage(restaurant)" half-icon="mdi-star-half" half-increments readonly ></v-rating></v-list-item>
+                    </v-col>
+                    <v-col cols="4" class="photo">
+                        <v-list-item ><v-img max-height="300" max-width="500" :src="restaurant[0].LogoUrl"></v-img></v-list-item> 
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="3" class="colCards">
+                        <v-list-item class="nomTypes"><font v-for="type in calculTypes(restaurant,[])" :key="type" class="nomType" >{{type}}</font></v-list-item>
+                    </v-col>
+                    <v-col cols="5" class="colDifLivreurs">
+                        <v-row>
+                            <v-col>
+                                <v-card-title>Délais : </v-card-title>
+                                <v-card-text>
+                                    <v-chip class="tempsLivraison" v-for="resto in restaurant" :key="resto.id">{{resto.Api}} : {{resto.DeliveryEtaMinutes.RangeLower}} - {{resto.DeliveryEtaMinutes.RangeUpper}} min</v-chip>
+                                </v-card-text>
+                            </v-col>
+                            <v-col>
+                                <v-card-title>Frais de livraison : </v-card-title>
+                                <v-card-text>
+                                    <v-chip class="tempsLivraison" v-for="resto in restaurant" :key="resto.id">{{resto.Api}} : {{resto.DeliveryCost}}{{devise}} </v-chip>
+                                </v-card-text>
+                            </v-col>
+                        </v-row>
+                    </v-col>
+                    <v-col  cols="3">
+                    </v-col>
+                </v-row>
+            </v-list-item-content>
+        </v-card>      
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "DashboardCard",
+        props: {
+            restaurant: Array, 
+            devise: String
+        },
+        methods : {
+            starAverage(sameRestaurants){
+                var average=0;
+                for(var restaurantNum in sameRestaurants){
+                    average=average+sameRestaurants[restaurantNum].Rating.StarRating
+                }
+                average=average/sameRestaurants.length
+                return average
+            },
+            calculTypes(sameRestaurants,res){
+                var typesSameRestaurant=[];
+                for(var restaurantNum in sameRestaurants){
+                    for(var typeNum in sameRestaurants[restaurantNum].CuisineTypes){
+                    typesSameRestaurant.push(sameRestaurants[restaurantNum].CuisineTypes[typeNum].Name)
+                    }
+                }
+                res = typesSameRestaurant.filter(this.listeUnique)
+                return res
+            },
+            listeUnique(value, index, self) {
+                return self.indexOf(value) === index;
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .listeCards{
+        text-align: left;
+        margin:10px;
+        width: 100%;
+    }
+
+    .nomRestaurant{
+        margin:20px;
+        margin-left: 40px;
+    }
+
+    .nomTypes{
+        margin-top:20%;
+        margin-left: 60px;
+        color:gray;
+    }
+
+    .nomType{
+        margin-right: 5px;
+    }
+
+    .colCards{
+        width:20%;
+    }
+
+    .notes{
+        margin-top: 20px;
+    }
+
+    .premiereLigne{
+        height: 30px;
+        width:100%;
+    }
+
+    .photo{
+        text-align: right;
+        padding:0px;
+        margin:0px;
+    }
+
+    .colDifLivreurs{
+        margin-top:40px;
+    }
+
+    .tempsLivraison{
+        margin:5px;
+    }
+</style>
