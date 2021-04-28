302151b2ebd5318b33ffb500a6d918a7cf0f6ff6
Florian Pont <florian1.pont@free.fr>
2021-04-28 12:06:42 +0200

    feat(dashboard): sort all closed restaurants at the end

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -337,7 +337,7 @@ export default {
         }
         allRestaurant.push(sameRestaurant);
       })
-      //allRestaurant.filter()
+      allRestaurant.sort(restaurant => { return restaurant[0].IsOpenNow ? -1 : 1 })
       this.allRestaurants = allRestaurant;
       this.filteredRestaurants = this.allRestaurants;
       this.affichageFiltre = true;
